ad5ab12aa4d49b73117c9af58136f0b3616b3e7f
[p].[b]_{123} <ipramit@gmail.com>
2026-08-25 23:52:37 -0400

    Add files via upload

---
+++ apps/terminator/config/terminator_config.py
@@ -0,0 +1,11 @@
+# apps/terminator/config/terminator_config.py
+
+BASE_URL = "https://terminator-app.example.com"
+
+USERNAME = "test_user@example.com"
+PASSWORD = "SuperSecret123"
+
+# Add other app-specific test data below as needed
+DEFAULT_TIMEOUT = 10
+TEXT_DATA_1 = ""
+TEXT_DATA_2 = ""
